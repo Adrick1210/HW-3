43b51c55914294c6778b17465ed237d94629c9a1
Christian Alexander <christian.alexanderato@gmail.com>
2023-11-04 23:10:33 -0700

    Methods Revisited Answered

--- answers.js
+++ answers.js
@@ -64,6 +64,52 @@ for (turtle of turtles) {
 // Methods Revisited
 //----------------------------------------
 
+const favMovies = [
+  "Jaws",
+  "The Fellowship of the Ring",
+  "Howl's Moving Castle",
+  "Django Unchained",
+  "Cloud Atlas",
+  "The Usual Suspects",
+  "Toy Story",
+  "Conan the Barbarian",
+  "Titanic",
+  "Harry Potter",
+  "Fried Green Tomatoes",
+  "Volver",
+  "Oculus",
+  "Seven",
+  "Black Panther",
+  "Harry Potter",
+  "Imitation of Life",
+  "Snatch",
+  "Fast and Furious",
+];
+
+console.log(favMovies[8]);
+favMovies.sort();
+// By using the .sort() method, it put the strings in the array in A-Z order. It seems to have permanently altered the app after the method
+// has been used, I tested a line of code after to see and it does not permanently affect the array with other methods until .sort() is called again.
+favMovies.push("Guardians of the Galaxy");
+favMovies.reverse();
+favMovies.shift();
+favMovies.unshift();
+// using the unshift() method, nothing returned because the object that was removed wasn't originally a part of the array (or, it wasn't hard-
+// coded).
+favMovies.splice(15, 1, "Avatar");
+// This splice will not permanently alter the array unless the next console.log() is called after this method.
+// favMovies.slice(9);
+// The slice does not permanently alter the array or do anything at the moment. It targets the last half of the array but does nothing until it is 
+// assigned.
+let favMovies2 = favMovies.slice(9);
+console.log(favMovies2);
+// It did not permanently alter our array as the .slice() method took half of the main array and stored inside a new variable that was called
+// with a console log.
+console.log(favMovies.indexOf("Fast and Furious"));
+// It is still in the original array because it has not been permanently altered. So by console.log and the index, we can still access the object.
+// It did not matter if we used "let" or "var" instead of "const". Because the objects were not manipulated using methods without bracket notations
+// of any kind. Using a "let" declaration would have resulted the same.
+
 //----------------------------------------
 // Where is Waldo
 //----------------------------------------
